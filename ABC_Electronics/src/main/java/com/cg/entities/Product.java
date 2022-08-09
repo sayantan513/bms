aeef8e12f6b04cafefc38601bf0830708dec7ca7
PraritKumar <88683171+PraritKumar@users.noreply.github.com>
2022-08-09 12:47:26 +0530

    Update Product.java

--- ABC_Electronics/src/main/java/com/cg/entities/Product.java
+++ ABC_Electronics/src/main/java/com/cg/entities/Product.java
@@ -1,5 +1,92 @@
 package com.cg.entities;
 
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Product {
+	@Column(name="model_number")
+	private @Id String modelNumber;
+	private String productName;
+	private String productCategoryName;
+	private LocalDate dateOfPurchase;
+	private int warrantyYears;
+	private LocalDate warrantyDate;
+	
+	public Product() {
+		
+	}
+	
+
+	public Product(String modelNumber, String productName, String productCategoryName, LocalDate dateOfPurchase,
+			int warrantyYears, LocalDate warrantyDate) {
+		super();
+		this.modelNumber = modelNumber;
+		this.productName = productName;
+		this.productCategoryName = productCategoryName;
+		this.dateOfPurchase = dateOfPurchase;
+		this.warrantyYears = warrantyYears;
+		this.warrantyDate = warrantyDate;
+	}
+
+
+	public String getModelNumber() {
+		return modelNumber;
+	}
+
+	public void setModelNumber(String modelNumber) {
+		this.modelNumber = modelNumber;
+	}
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+	public String getProductCategoryName() {
+		return productCategoryName;
+	}
+
+	public void setProductCategoryName(String productCategoryName) {
+		this.productCategoryName = productCategoryName;
+	}
+
+	public LocalDate getDateOfPurchase() {
+		return dateOfPurchase;
+	}
+
+	public void setDateOfPurchase(LocalDate dateOfPurchase) {
+		this.dateOfPurchase = dateOfPurchase;
+	}
+
+	public int getWarrantyYears() {
+		return warrantyYears;
+	}
+
+	public void setWarrantyYears(int warrantyYears) {
+		this.warrantyYears = warrantyYears;
+	}
+
+	public LocalDate getWarrantyDate() {
+		return warrantyDate;
+	}
+
+	public void setWarrantyDate(LocalDate warrantyDate) {
+		this.warrantyDate = warrantyDate;
+	}
+
+	@Override
+	public String toString() {
+		return "Product [modelNumber=" + modelNumber + ", productName=" + productName + ", productCategoryName="
+				+ productCategoryName + ", dateOfPurchase=" + dateOfPurchase + ", warrantyYears=" + warrantyYears
+				+ ", warrantyDate=" + warrantyDate + "]";
+	}
+	
 
 }
