5be9cfd8d6e324839bce2af8dfefcb867959695f
Siddharth Gautam <60943685+sidgautam0000@users.noreply.github.com>
2022-08-09 15:11:26 +0530

    Update IClientService.java

--- ABC_Electronics/src/main/java/com/cg/service/IClientService.java
+++ ABC_Electronics/src/main/java/com/cg/service/IClientService.java
@@ -1,5 +1,22 @@
 package com.cg.service;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entities.Client;
+import com.cg.entities.Engineer;
+
+
+@RestController
 public interface IClientService {
 
+    void saveClient(Client c);
+    Client getClientById(int i);
+    Engineer getEngineerById(int i);
+    List<Engineer> getEngineersByDomain(String s);
+    String ChangeStatusOfComplaint(int i);
+    Client signIn(Client c);
+    Client signOut(Client c);
+        
 }
