9e5aeeb1e8a173e771447795be3c7c05a153ff4c
Siddharth Gautam <60943685+sidgautam0000@users.noreply.github.com>
2022-08-09 15:48:48 +0530

    Update IComplaintService.java

--- ABC_Electronics/src/main/java/com/cg/service/IComplaintService.java
+++ ABC_Electronics/src/main/java/com/cg/service/IComplaintService.java
@@ -1,5 +1,23 @@
 package com.cg.service;
 
+import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entities.Client;
+import com.cg.entities.Engineer;
+import com.cg.entities.Product;
+import com.cg.entities.Complaint;
+
+@RestController
 public interface IComplaintService {
 
+    
+    boolean bookComplaint(Client c,Complaint com,Product p);
+    String changeComplaintStatus(Complaint com);
+    List<Complaint> getClientAllComplaints(Client c);
+    List<Complaint> getClientAllOpenComplaints(Client c);
+    Engineer getEngineer(int i);
+    Product getProductByComplaint(int i);
+    
 }
+
