710c4942acd46daec813496cd11fbe3d6b3cb018
sayantan513 <98946995+sayantan513@users.noreply.github.com>
2022-08-09 15:58:53 +0530

    Update IProductService.java
    product service updated

--- ABC_Electronics/src/main/java/com/cg/service/IProductService.java
+++ ABC_Electronics/src/main/java/com/cg/service/IProductService.java
@@ -1,5 +1,21 @@
 package com.cg.service;
 
+import java.util.List;
+
+import com.cg.entities.Engineer;
+import com.cg.entities.Product;
+
+
+
+
+
 public interface IProductService {
 
+	public void addProduct();
+	public void removeProducts(String s);
+	public List<Product> getProduct(String s);
+	public void updateProductWarranty(String s);
+	public List<Product> getProductComplaints(String s);
+	public List<Engineer> getEngineersByProduct(String s);
+	
 }
