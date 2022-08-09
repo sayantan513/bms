d71c233959a7c62effaede969a71ca2330a595ed
PraritKumar <88683171+PraritKumar@users.noreply.github.com>
2022-08-09 14:52:47 +0530

    Update IAdminRepository.java

--- ABC_Electronics/src/main/java/com/cg/repository/IAdminRepository.java
+++ ABC_Electronics/src/main/java/com/cg/repository/IAdminRepository.java
@@ -1,5 +1,10 @@
 package com.cg.repository;
 
-public interface IAdminRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.cg.entities.Admin;
+
+public interface IAdminRepository extends JpaRepository<Admin,Integer> {
+
+	
 }
