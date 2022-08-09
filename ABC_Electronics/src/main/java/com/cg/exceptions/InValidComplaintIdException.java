e7310c70f3a63a4fc3a74822b909e385addfc1d8
shashwat789 <54533850+shashwat789@users.noreply.github.com>
2022-08-09 15:50:56 +0530

    Update InValidComplaintIdException.java

--- ABC_Electronics/src/main/java/com/cg/exceptions/InValidComplaintIdException.java
+++ ABC_Electronics/src/main/java/com/cg/exceptions/InValidComplaintIdException.java
@@ -3,6 +3,6 @@ package com.cg.exceptions;
 public class InValidComplaintIdException extends Exception {
 
 	public InValidComplaintIdException(String message) {
-		super(message);
+		 super(message);
 	}
 }
