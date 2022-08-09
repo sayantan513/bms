ec8e74db4af48e226ea9e4fdbce1f3d2e31798a9
Siddharth Gautam <60943685+sidgautam0000@users.noreply.github.com>
2022-08-09 15:53:56 +0530

    Update IAdminService.java

--- ABC_Electronics/src/main/java/com/cg/service/IAdminService.java
+++ ABC_Electronics/src/main/java/com/cg/service/IAdminService.java
@@ -1,5 +1,20 @@
 package com.cg.service;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.cg.entities.Complaint;
+import com.cg.entities.Engineer;
+import com.cg.entities.Admin;
+
 public interface IAdminService {
 
+    void addEngineer(Engineer e);
+    void changeDomain(int i,String s);
+    void removeEngineer(int i);
+    List<Complaint> getComplaintsByProducts(String s);
+    List<Complaint> getComplaints(String a,String b);
+    Complaint replaceEngineerFromComplaint(int i);
+    
 }
