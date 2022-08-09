1e45e8af32ac32b7d5069e5b4c13698d3db18032
shashwat789 <54533850+shashwat789@users.noreply.github.com>
2022-08-09 15:42:15 +0530

    Update IEngineerRepository.java

--- ABC_Electronics/src/main/java/com/cg/repository/IEngineerRepository.java
+++ ABC_Electronics/src/main/java/com/cg/repository/IEngineerRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.cg.entities.Engineer;
 
 @Repository
-public interface IEngineerRepository extends JpaRepository<Engineer,Integer> {
+public interface IEngineerRepository extends JpaRepository<Engineer, Integer> {
 
 }
