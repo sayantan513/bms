e1bd73f8a78c68e31b2357f102690f6026b4d799
PraritKumar <88683171+PraritKumar@users.noreply.github.com>
2022-08-09 12:43:45 +0530

    Update Client.java

--- ABC_Electronics/src/main/java/com/cg/entities/Client.java
+++ ABC_Electronics/src/main/java/com/cg/entities/Client.java
@@ -1,5 +1,68 @@
 package com.cg.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Client {
+	@Column(name="client_id")
+	private @Id String clientId;
+	private String password;
+	private String address;
+	private long phoneNumber;
+	
+	public Client() {
+		
+	}
+	
+
+	public Client(String clientId, String password, String address, long phoneNumber) {
+		this.clientId = clientId;
+		this.password = password;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+	}
+
+
+	public String getClientId() {
+		return clientId;
+	}
+
+	public void setClientId(String clientId) {
+		this.clientId = clientId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "Client [clientId=" + clientId + ", password=" + password + ", address=" + address + ", phoneNumber="
+				+ phoneNumber + "]";
+	}
+	
 
 }
+
