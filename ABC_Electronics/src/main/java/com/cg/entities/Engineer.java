5974a20dc1d1d61bec6e162a50ecf431732e24fb
PraritKumar <88683171+PraritKumar@users.noreply.github.com>
2022-08-09 12:46:40 +0530

    Update Engineer.java

--- ABC_Electronics/src/main/java/com/cg/entities/Engineer.java
+++ ABC_Electronics/src/main/java/com/cg/entities/Engineer.java
@@ -1,5 +1,66 @@
 package com.cg.entities;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class Engineer {
+	@Column(name="employee_Id")
+	private @Id Integer employeeId;
+	private String password;
+	private String engineerName;
+	private String domain;
+	
+	public Engineer() {
+		
+	}
+	
+	public Engineer(Integer employeeId, String password, String engineerName, String domain) {
+		this.employeeId = employeeId;
+		this.password = password;
+		this.engineerName = engineerName;
+		this.domain = domain;
+	}
+
+
+	public int getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(Integer employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEngineerName() {
+		return engineerName;
+	}
+
+	public void setEngineerName(String engineerName) {
+		this.engineerName = engineerName;
+	}
+
+	public String getDomain() {
+		return domain;
+	}
+
+	public void setDomain(String domain) {
+		this.domain = domain;
+	}
+
+	@Override
+	public String toString() {
+		return "Engineer [employeeId=" + employeeId + ", password=" + password + ", engineerName=" + engineerName
+				+ ", domain=" + domain + "]";
+	}
+	
 
 }
